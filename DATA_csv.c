118fc3b1759314007bdd34c02bdc11dd27b7bb8f
Minjae Isaac Kwon <minals3165@gmail.com>
2017-06-07 20:19:30 +0900

    Reject diff

--- DATA_csv.c
+++ DATA_csv.c
@@ -25,7 +25,7 @@ void make_file()
 //DHT22
 void read_dht_data()
 {
-measurseq:
+//measurseq:
 	uint8_t laststate    = HIGH;
 	uint8_t counter      = 0;
  	uint8_t j            = 0, i;
@@ -100,7 +100,7 @@ measurseq:
 	}
 	else 
     { goto measurseq;
-/*		printf( "Data not good, skip\n" );
+		printf( "Data not good, skip\n" );
 
                 //시간측정
                 time_t timer;
@@ -112,7 +112,7 @@ measurseq:
                 FILE* fp = fopen(fileD,"a");
                 fprintf(fp,"%d/%d/%d-%d:%d,,",t->tm_year + 1900,t->tm_mon +1, t->tm_mday, t->tm_hour, t->tm_min);
                 fclose(fp);
-*/
+
 
 	}
 }
