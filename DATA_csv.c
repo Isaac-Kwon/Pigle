ac76d9f36049a02cb30074746618447ca906d8f9
Minjae Isaac Kwon <minals3165@gmail.com>
2017-06-07 20:15:14 +0900

    Sequence Add
    Remeasure when data is not good @ TH Senser

--- DATA_csv.c
+++ DATA_csv.c
@@ -25,6 +25,7 @@ void make_file()
 //DHT22
 void read_dht_data()
 {
+    measurseq:
 	uint8_t laststate    = HIGH;
 	uint8_t counter      = 0;
  	uint8_t j            = 0, i;
@@ -98,8 +99,8 @@ void read_dht_data()
 
 	}
 	else 
-	{
-		printf( "Data not good, skip\n" );
+    { goto measurseq
+/*		printf( "Data not good, skip\n" );
 
                 //시간측정
                 time_t timer;
@@ -111,7 +112,7 @@ void read_dht_data()
                 FILE* fp = fopen(fileD,"a");
                 fprintf(fp,"%d/%d/%d-%d:%d,,",t->tm_year + 1900,t->tm_mon +1, t->tm_mday, t->tm_hour, t->tm_min);
                 fclose(fp);
-
+*/
 
 	}
 }
