7e3655190973c5eb04c6efac66a90f23faa5e22c
Minjae Isaac Kwon <minals3165@gmail.com>
2017-06-13 16:56:35 +0900

    Status Inspection

--- status_update.py
+++ status_update.py
@@ -28,7 +28,28 @@ def num(s):
         return float(s)
 
 
-nowtime = nowdata[1]
-nowtemp = num(nowdata[2])
-nowhum = num(nowdata[3])
+nowtime = nowdata[0]
+nowtemp = num(nowdata[1])
+nowhum = num(nowdata[2])
+
+#===================================================#
+
+#data inspection (nowdata)
+inspect_temp = [0,num(data["hysteresis"]["low"]["send"]["temperature"]), num(data["hysteresis"]["low"]["reset"]["temperature"]), num(data["hysteresis"]["high"]["reset"]["temperature"]), num(data["hysteresis"]["low"]["send"]["temperature"]) ,100]
+inspect_hum = [0,num(data["hysteresis"]["low"]["send"]["humidity"]), num(data["hysteresis"]["low"]["reset"]["humidity"]), num(data["hysteresis"]["high"]["reset"]["humidity"]), num(data["hysteresis"]["low"]["send"]["humidity"]) ,100]
+
+for i in range(5):
+    ins_low = inspect_temp[i]
+    ins_high = inspect_temp[i+1]
+    #
+    if ins_low<nowtemp and ins_high>nowtemp:
+        instemp = i
+
+for i in range(5):
+    ins_low = inspect_hum[i]
+    ins_high = inspect_hum[i+1]
+    #
+    if ins_low<nowhum and ins_high>nowhum:
+        inshum = i
+
 
