f7ba7abf415ab5bc95e7505a3b3adc72b297a674
Minjae Isaac Kwon <minals3165@gmail.com>
2017-06-14 16:51:47 +0900

    Comparison Update

--- status_update.py
+++ status_update.py
@@ -1,3 +1,5 @@
+# -*-coding: utf-8 -*-
+
 import csv
 import json
 
@@ -69,78 +71,82 @@ for i in range(5):
 
 ##temperature
 
-if data["log"]["status"]["temperature"] == "high" and (istemp in [1]):
+templog_status = [0,0,0,0,0,0] #High Low 시작, 종료, 지속, Low Log 시작, 종료, 지속
+
+if data["log"]["status"]["temperature"] == "high" and (instemp in [1]):
     # 이전 상태가 HIGH LOGGING 상태이면서, 현태 상태가 LOW 인 경우
     # HIGH LOGGING 을 중단하고, LOW LOGGING 을 시작함
-    #
-elif data["log"]["status"]["temperature"] == "high" and (istemp in [2,3]):
+    templog_status = [0,1,0,1,0,0]
+elif data["log"]["status"]["temperature"] == "high" and (instemp in [2,3]):
     # 이전 상태가 HIGH LOGGING 상태이면서, 현태 상태가 Normal 인 경우
-    # LOGGING 을 중단함.
-    #
-elif data["log"]["status"]["temperature"] == "high" and (istemp in [4,5]):
+    # HIGH LOGGING 을 중단함.
+    templog_status = [0,1,0,0,0,0]
+elif data["log"]["status"]["temperature"] == "high" and (instemp in [4,5]):
     # 이전 상태가 HIGH LOGGING 상태이면서, 현태 상태가 High Hyst 또는 High 인 경우
-    # LOGGING 을 계속함.
-    #
-elif data["log"]["status"]["temperature"] == "normal" and (istemp in [1]):
+    # HIGH LOGGING 을 계속함.
+    templog_status = [0,0,1,0,0,0]
+elif data["log"]["status"]["temperature"] == "normal" and (instemp in [1]):
     # 이전 상태가 normal_unlogging 상태이면서, 현태 상태가 Low 인 경우
     # LOGGING을 시작함 (Low)
-    #
-elif data["log"]["status"]["temperature"] == "normal" and (istemp in [2,3,4]):
+    templog_status = [0,0,0,1,0,0]
+elif data["log"]["status"]["temperature"] == "normal" and (instemp in [2,3,4]):
     # 이전 상태가 normal_unlogging 상태이면서, 현태 상태가 Normal, High Hyst, Low Hyst 인 경우
     # LOGGING 을 안하고 있는 것을 지속함.
-    #
-elif data["log"]["status"]["temperature"] == "normal" and (istemp in [5]):
+    templog_status = [0,0,0,0,0,0]
+elif data["log"]["status"]["temperature"] == "normal" and (instemp in [5]):
     # 이전 상태가  normal_unlogging 상태이면서, 현태 상태가 High 인 경우
-    # LOGGING 을 시작함 (High)
-    #
-elif data["log"]["status"]["temperature"] == "low" and (istemp in [1,2]):
+    # HIGH LOGGING 을 시작함 (High)
+    templog_status = [1,0,0,0,0,0]
+elif data["log"]["status"]["temperature"] == "low" and (instemp in [1,2]):
     # 이전 상태가 LOW LOGGING 상태이면서, 현태 상태가 Low, Low Hyst 인 경우
-    # LOGGING 을 계속함.
-    #
-elif data["log"]["status"]["temperature"] == "low" and (istemp in [3,4]):
+    # LOW LOGGING 을 계속함.
+    templog_status = [0,0,0,0,0,1]
+elif data["log"]["status"]["temperature"] == "low" and (instemp in [3,4]):
     # 이전 상태가 LOW LOGGING 상태이면서, 현태 상태가 Normal, High Hyst 인 경우
-    # LOGGING 을 중단함.
-    #
-elif data["log"]["status"]["temperature"] == "low" and (istemp in [5]):
+    # LOW LOGGING 을 중단함.
+    templog_status = [0,0,0,0,0,0]
+elif data["log"]["status"]["temperature"] == "low" and (instemp in [5]):
     # 이전 상태가 LOW LOGGING 상태이면서, 현태 상태가 High 인 경우
     # LOW LOGGING 을 중단하고, HIGH LOGGING 을 시작함.
-    #
+    templog_status = [1,0,0,0,1,0]
 
 ##humidity
 
-if data["log"]["status"]["humidity"] == "high" and (istemp in [1]):
+humlog_status = [0,0,0,0,0,0] #High Low 시작, 종료, 지속, Low Log 시작, 종료, 지속
+
+if data["log"]["status"]["humidity"] == "high" and (inshum in [1]):
     # 이전 상태가 HIGH LOGGING 상태이면서, 현태 상태가 LOW 인 경우
     # HIGH LOGGING 을 중단하고, LOW LOGGING 을 시작함
-    #
-elif data["log"]["status"]["humidity"] == "high" and (istemp in [2,3]):
+    humlog_status = [0,1,0,1,0,0]
+elif data["log"]["status"]["humidity"] == "high" and (inshum in [2,3]):
     # 이전 상태가 HIGH LOGGING 상태이면서, 현태 상태가 Normal 인 경우
-    # LOGGING 을 중단함.
-    #
-elif data["log"]["status"]["humidity"] == "high" and (istemp in [4,5]):
+    # HIGH LOGGING 을 중단함.
+    humlog_status = [0,1,0,0,0,0]
+elif data["log"]["status"]["humidity"] == "high" and (inshum in [4,5]):
     # 이전 상태가 HIGH LOGGING 상태이면서, 현태 상태가 High Hyst 또는 High 인 경우
-    # LOGGING 을 계속함.
-    #
-elif data["log"]["status"]["humidity"] == "normal" and (istemp in [1]):
+    # HIGH LOGGING 을 계속함.
+    humlog_status = [0,0,1,0,0,0]
+elif data["log"]["status"]["humidity"] == "normal" and (inshum in [1]):
     # 이전 상태가 normal_unlogging 상태이면서, 현태 상태가 Low 인 경우
     # LOGGING을 시작함 (Low)
-    #
-elif data["log"]["status"]["humidity"] == "normal" and (istemp in [2,3,4]):
+    humlog_status = [0,0,0,1,0,0]
+elif data["log"]["status"]["humidity"] == "normal" and (inshum in [2,3,4]):
     # 이전 상태가 normal_unlogging 상태이면서, 현태 상태가 Normal, High Hyst, Low Hyst 인 경우
     # LOGGING 을 안하고 있는 것을 지속함.
-    #
-elif data["log"]["status"]["humidity"] == "normal" and (istemp in [5]):
+    humlog_status = [0,0,0,0,0,0]
+elif data["log"]["status"]["humidity"] == "normal" and (inshum in [5]):
     # 이전 상태가  normal_unlogging 상태이면서, 현태 상태가 High 인 경우
-    # LOGGING 을 시작함 (High)
-    #
-elif data["log"]["status"]["humidity"] == "low" and (istemp in [1,2]):
+    # HIGH LOGGING 을 시작함 (High)
+    humlog_status = [1,0,0,0,0,0]
+elif data["log"]["status"]["humidity"] == "low" and (inshum in [1,2]):
     # 이전 상태가 LOW LOGGING 상태이면서, 현태 상태가 Low, Low Hyst 인 경우
-    # LOGGING 을 계속함.
-    #
-elif data["log"]["status"]["humidity"] == "low" and (istemp in [3,4]):
+    # LOW LOGGING 을 계속함.
+    humlog_status = [0,0,0,0,0,1]
+elif data["log"]["status"]["humidity"] == "low" and (inshum in [3,4]):
     # 이전 상태가 LOW LOGGING 상태이면서, 현태 상태가 Normal, High Hyst 인 경우
-    # LOGGING 을 중단함.
-    #
-elif data["log"]["status"]["humidity"] == "low" and (istemp in [5]):
+    # LOW LOGGING 을 중단함.
+    humlog_status = [0,0,0,0,0,0]
+elif data["log"]["status"]["humidity"] == "low" and (inshum in [5]):
     # 이전 상태가 LOW LOGGING 상태이면서, 현태 상태가 High 인 경우
     # LOW LOGGING 을 중단하고, HIGH LOGGING 을 시작함.
     #
